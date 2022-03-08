6279f735a47185377813a54d1a93e49fda13d713
Alvaro Sirvent <alvaro.sirvent@global.ntt>
2022-03-08 02:40:08 +0100

    Added minor changes

--- tech_chain/library_chain/library_chain/api/chain.py
+++ tech_chain/library_chain/library_chain/api/chain.py
@@ -4,4 +4,13 @@ class Chain:
 
     def __init__(self): 
         self.size_per_block = 1000 
+        self.blocks = []
         return
+    
+   
+
+
+    def add_block(self, block):
+        if self.blocks[-1].hash != block.previous_hash:
+            return False
+        self.blocks.append(block)
--- tech_chain/library_chain/library_chain/models/block.py
+++ tech_chain/library_chain/library_chain/models/block.py
@@ -3,11 +3,23 @@ from library_chain.models import MerkleRoot
 
 class Block: 
 
-    def __init__(self, index, neural_data_transaction, timestamp ):
+    def __init__(self, index, neural_data_transaction, timestamp, previous_hash ):
         self.index = index
         self.neural_data_transaction = neural_data_transaction 
         self.timestamp = timestamp
+        self.previous_hash = previous_hash
+        self.mkrlroot = MerkleRoot(neural_data_transaction)
+        self.hash = self.set_hash()
         return 
+
+    def set_hash(self):
+        return self.get_hash( list( self.previous_hash, self.mkrlroot))
+    
+    #Function that calculate the hash from one transaction in a whole block
+    def get_hash (self, data ): 
+        return sha256(json.dumps(data.__dict__, sort_keys= True).encode()).hexdigest()
+
+    
 
     
     
